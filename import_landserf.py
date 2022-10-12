510fd58c520a917fbbe6b38b2da1a530769ad9cd
dr. Kertész Csaba-Zoltán <csaba.kertesz@unitbv.ro>
2022-10-12 11:43:02 +0300

    add MIT license

--- import_landserf.py
+++ import_landserf.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
+QGIS Processing algorithm for importing Landserf summits
 """
+__author__ = "dr. Kertész Csaba Zoltán"
+__email__ = "csaba.kertesz@unitbv.ro"
+__copyright__ = "Copyright 2022, dr. Kertész Csaba-Zoltán"
+__license__ = "MIT"
 
 from qgis.PyQt.QtCore import (QCoreApplication,
                               QVariant)
